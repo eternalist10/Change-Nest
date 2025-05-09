210eff84d713daa3e2f9937f5d365530ae256ff7
Shubhraneel Chakraborty <chakrabortyshubhraneel@gmail.com>
2025-05-09 11:44:29 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -115,9 +115,7 @@ const Navbar = () => {
               styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
               handleClick={() => {
                 if (address) navigate("create-campaign");
-                else connect();
-              }}
-            ></CustomButton>
+                else connect();}}></CustomButton>
           </div>
         </div>
       </div>
